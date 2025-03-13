8e36530fa4ac7b4eb3448a72f10c7903f81194cd
shruti <nikamshruti27@gmail.com>
2025-03-13 13:57:16 +0530

    changes

--- components/Mobile-header.tsx
+++ components/Mobile-header.tsx
@@ -1,3 +1,7 @@
 export const MobileHeader = () => {
-    return
+    return (
+        <nav className="lg:hidden px-6 h-[50px] flex items-center by-yellow-500 border-b fixed top-0 w-full z-50">
+            Mobile
+        </nav>
+    )
 }
