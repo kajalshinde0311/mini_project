699a5364c3f0ecb9aed2bfd23f6a0c3cd212017c
Kaunil <167971412+Kaunil@users.noreply.github.com>
2025-03-20 13:26:58 +0530

    chnge

---
+++ components/db/schema.ts
@@ -0,0 +1,7 @@
+import { fromTheme } from "tailwind-merge";
+import { pgTable, serial, text} from "drizzle-orm/pg-core";
+export const courses =pgTable
+("courses" ,{
+  id:serial("id").primaryKey(),
+  title:text("")  
+})
